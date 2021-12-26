28b52e684262243a5b15ccf1d3788063a6ebc38f
Oliver Gašpar <oliver.g.sk@gmail.com>
2021-12-26 04:25:15 +0100

    Added cancellation support for loading videos when changing video sources

--- Footage/ViewModel/Section/VideoBrowserViewModel.cs
+++ Footage/ViewModel/Section/VideoBrowserViewModel.cs
@@ -13,7 +13,8 @@
     public class VideoBrowserViewModel : SectionViewModel
     {
         private MediaSource? selectedSource;
-        
+
+        private TaskCompletionSource<bool>? currentFetchTcs;
 
         public ObservableCollection<VideoViewModel> Videos { get; }
 
@@ -42,27 +43,49 @@
             
             // TODO clear async
             Videos.Clear();
-            
+
             // TODO cancel existing, if any
-            Task.Run(() => FetchVideos());
+            Task.Run(async () =>
+            {
+                if (currentFetchTcs != null)
+                {
+                    await currentFetchTcs.Task;
+                }
+                
+                return FetchVideos();
+            });
         }
 
         private async Task FetchVideos(int? batchSize = null)
         {
+            var initialSource = selectedSource;
+            
+            currentFetchTcs = new TaskCompletionSource<bool>();
+            
             using var repo = new VideoBrowserRepository();
             
             var videos = repo.FetchVideos(selectedSource, batchSize);
 
             foreach (var video in videos)
             {
-                await Dispatcher.UIThread.InvokeAsync(async () =>
+                await Dispatcher.UIThread.InvokeAsync(() =>
                 {
-#if DEBUG
-                    await Task.Delay(250);
-#endif
                     Videos.Add(new VideoViewModel(video));
                 });
+                
+                if (selectedSource != initialSource)
+                {
+                    Videos.Clear(); // TODO clear async
+                    break;
+                }
+                
+#if DEBUG
+                await Task.Delay(250);
+#endif
             }
+
+            currentFetchTcs.TrySetResult(true);
+            currentFetchTcs = null;
         }
     }
 }
