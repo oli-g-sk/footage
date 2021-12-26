a157206a48fc4acdfc5516585506eefab68fb9bc
Oliver Gašpar <oliver.g.sk@gmail.com>
2021-12-26 16:21:07 +0100

    The Entity base model class overrides Equals and == operator comparing on the primary key

--- Footage/Model/Entity.cs
+++ Footage/Model/Entity.cs
@@ -2,6 +2,36 @@
 {
     public abstract class Entity
     {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
         public int Id { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Entity) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+        public static bool operator ==(Entity? left, Entity? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Entity? left, Entity? right)
+        {
+            return !Equals(left, right);
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        protected bool Equals(Entity other)
+        {
+            return Id == other.Id;
+        }
     }
 }
