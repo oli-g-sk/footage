1747549a8ed527f8709d49b3f090e3f3a4732dfd
Oliver Gašpar <oliver.g.sk@gmail.com>
2022-01-22 15:51:04 +0100

    Added VLC logging

--- Footage/Service/MediaPlayerService.cs
+++ Footage/Service/MediaPlayerService.cs
@@ -3,9 +3,14 @@
     using System;
     using System.Threading.Tasks;
     using LibVLCSharp.Shared;
+    using LogLevel = LibVLCSharp.Shared.LogLevel;
+    using NLogLevel = NLog.LogLevel;
+    using NLog;
 
     public sealed class MediaPlayerService : IMediaPlayerService, IDisposable
     {
+        private static ILogger VlcLog => LogManager.GetLogger("LibVLC");
+
         private static readonly LibVLC LibVlc;
 
         private readonly MediaPlayer helperPlayer;
@@ -18,8 +23,35 @@
         {
             Core.Initialize();
             LibVlc = new LibVLC();
+            LibVlc.Log += LibVlc_Log;
         }
-        
+
+        private static void LibVlc_Log(object? sender, LogEventArgs e)
+        {
+            NLogLevel? nLogLevel = null;
+
+            switch (e.Level)
+            {
+                case LogLevel.Debug:
+                    nLogLevel = NLogLevel.Trace;
+                    break;
+                case LogLevel.Notice:
+                    nLogLevel = NLogLevel.Info;
+                    break;
+                case LogLevel.Warning:
+                    nLogLevel = NLogLevel.Warn;
+                    break;
+                case LogLevel.Error:
+                    nLogLevel= NLogLevel.Error;
+                    break;
+            }
+
+            if (nLogLevel != null)
+            {
+                VlcLog.Log(nLogLevel, e.Message);
+            }
+        }
+
         public MediaPlayerService()
         {
             Player = new MediaPlayer(LibVlc);
