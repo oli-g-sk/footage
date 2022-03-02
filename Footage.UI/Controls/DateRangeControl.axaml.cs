602cccc1708d5f5e6467c0ddedb3fc0685531ba3
Oliver Gašpar <oliver.g.sk@gmail.com>
2022-03-02 15:38:05 +0100

    Day input uses NumericUpDown

--- Footage.UI/Controls/DateRangeControl.axaml.cs
+++ Footage.UI/Controls/DateRangeControl.axaml.cs
@@ -4,6 +4,9 @@ using Avalonia.Markup.Xaml;
 
 namespace Footage.UI.Controls
 {
+    using System.Diagnostics;
+    using Avalonia.Controls.Primitives;
+
     public class DateRangeControl : UserControl
     {
         public DateRangeControl()
@@ -14,6 +17,16 @@ namespace Footage.UI.Controls
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void Slider_PropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            var slider = sender as Slider;
+
+            if (e.Property == RangeBase.ValueProperty)
+            {
+                Debug.WriteLine(e.NewValue);
+            }
         }
     }
 }
