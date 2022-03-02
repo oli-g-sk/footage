e5ba42106dc12e7201427dc3ff17953f9d1637b9
Oliver Gašpar <oliver.g.sk@gmail.com>
2022-03-02 14:05:31 +0100

    FOO-72 Initial DateRangeControl mockup

---
+++ Footage.UI/Controls/DateRangeControl.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Footage.UI.Controls
+{
+    public class DateRangeControl : UserControl
+    {
+        public DateRangeControl()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
