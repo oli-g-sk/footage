3b7ef68534c97ce60c8e7bf9fb96204bfe35b204
Oliver Gašpar <oliver.g.sk@gmail.com>
2022-03-03 18:24:43 +0100

    Working "scrolled to end" detection in video grid

--- Footage.UI/Views/MainWindowContent/VideoBrowserView.axaml.cs
+++ Footage.UI/Views/MainWindowContent/VideoBrowserView.axaml.cs
@@ -1,14 +1,65 @@
 ﻿namespace Footage.UI.Views.MainWindowContent
 {
+    using System;
+    using System.Diagnostics;
+    using System.Reactive.Disposables;
+    using System.Reactive.Linq;
+    using Avalonia;
     using Avalonia.Controls;
+    using Avalonia.Controls.Mixins;
     using Avalonia.Markup.Xaml;
 
     public class VideoBrowserView : UserControl
     {
+        private readonly CompositeDisposable disposables = new();
+
+        private CompositeDisposable? scrollViewerDisposables;
+        
+        private double verticalHeightMax;
+
         public VideoBrowserView()
         {
             InitializeComponent();
+            
+            var listBox = this.Find<ListBox>("ListBox");
+            listBox.GetObservable(ListBox.ScrollProperty)
+                .OfType<ScrollViewer>()
+                .Take(1)
+                .Subscribe(sv =>
+                {
+                    scrollViewerDisposables?.Dispose();
+                    scrollViewerDisposables = new CompositeDisposable();
+                    
+                    sv.GetObservable(ScrollViewer.VerticalScrollBarMaximumProperty)
+                        .Subscribe(newMax => verticalHeightMax = newMax)
+                        .DisposeWith(scrollViewerDisposables);
+
+                    sv.GetObservable(ScrollViewer.OffsetProperty)
+                        .Subscribe(offset =>
+                        {
+                            if (offset.Y <= Double.Epsilon)
+                            {
+                                Debug.WriteLine("At Top");
+                            }
+
+                            var delta = Math.Abs(verticalHeightMax - offset.Y);
+                            if (delta <= Double.Epsilon)
+                            {
+                                Debug.WriteLine("At Bottom");
+                                // TODO fetch videos
+                            }
+                        }).DisposeWith(disposables);
+                });
         }
+        
+        /* TODO dispose
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            _scrollViewerDisposables.Dispose();
+            _disposables.Dispose();
+        }
+        */
 
         private void InitializeComponent()
         {
